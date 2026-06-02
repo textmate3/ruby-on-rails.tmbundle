607df0a8c45e83010cdfe59552a50c0905d17133
Shane Becker <veganstraightedge@gmail.com>
2026-06-02 01:29:03 -0700

    URI.escape -> URI::DEFAULT_PARSER.escape (URI.escape removed in Ruby 3.0)

--- Support/lib/rails/text_mate.rb
+++ Support/lib/rails/text_mate.rb
@@ -16,7 +16,7 @@ module TextMate
     def open(filename, line_number = nil, column_number = nil)
       filename = filename.filepath if filename.is_a? RailsPath
       options = []
-      options << "url=file://#{URI.escape(filename)}"
+      options << "url=file://#{URI::DEFAULT_PARSER.escape(filename)}"
       options << "line=#{line_number + 1}" if line_number
       options << "column=#{column_number + 1}" if column_number
       open_url "txmt://open?" + options.join("&")
